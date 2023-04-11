10312c231f389a9db29e934a020c08647b5ba953
Aaron William Po <apo2@uwo.ca>
2023-04-11 19:39:20 -0400

    Feat: Implement next.js font optimization.
    Also bumped next.js to version 13.3.0.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,14 @@ import CustomToaster from '@/components/toaster';
 import Drawer from '@/components/drawer';
 import { SessionProvider } from 'next-auth/react';
 
+// using Lato as an example font, feel free to use whatever font you want.
+
+import { Lato } from 'next/font/google';
+const lato = Lato({
+	weight: ['100', '300', '400', '700', '900'],
+	subsets: ['latin'],
+});
+
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	// ? Used to fix some hydration issue for themeChange
 	useEffect(() => {
@@ -16,6 +24,14 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 
 	return (
 		<>
+			<style jsx global>{
+				// use whatever font you want
+				`
+					html {
+						font-family: ${lato.style.fontFamily};
+					}
+				`
+			}</style>
 			<SessionProvider session={session}>
 				<Head>
 					<title>Next.js Template</title>
